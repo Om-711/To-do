11fa3e068e37be64faa3d513747bf90d4326adf7
Om-711 <omchiddarwar1245@gmail.com>
2026-03-12 02:11:36 +0530

    Commit

---
+++ frontend/script.js
@@ -0,0 +1,30 @@
+async function addUser(){
+    const name = document.getElementById("name").value
+    const email = document.getElementById("email").value
+
+    const response = await axios.post("http://localhost:3000/user",{
+        name : name, 
+        email : email
+    });
+
+    if(response.data.message === "Work"){
+        alert("User created")
+    }
+
+    localStorage.setItem("user", JSON.stringify(response.data))
+    window.location.href = "./todo.html?userID=" + response.data.userId
+}
+
+
+async function DisplayUser(){
+    const response = await axios.get("http://localhost:3000/getuser")
+    const users = response.data.user
+    console.log(users)
+    const div = document.getElementById("list")
+    let list = ""
+    for(let i=0;i<users.length;i++){
+        list += "Name: " + users[i].name + "<br>Email-ID: " + users[i].email + "<br><br>";
+    }
+
+    div.innerHTML = list
+};
---
+++ frontend/todoscipt.js
@@ -0,0 +1,79 @@
+// async function todo(){
+
+//     const input = document.getElementById("text")
+//     const text = input.value
+
+//     const li = document.createElement("li")
+//     const checkbox = document.createElement("input")
+//     checkbox.type = "checkbox"
+
+//     const label = document.createElement("span")
+//     label.textContent = " " + text
+
+//     checkbox.addEventListener("change", function(){
+//         if(checkbox.checked){
+//             li.remove()
+//         }
+//     });
+
+//     li.append(checkbox)
+//     li.append(label)
+//     document.getElementById("task-list").append(li)
+
+// }
+
+const params = new URLSearchParams(window.location.search)
+const savedUser = JSON.parse(localStorage.getItem("user") || "null")
+const userID = params.get("userID") || savedUser?.userId
+
+
+async function loadTask(){
+    const response = await axios.get("http://localhost:3000/tasks/" + userID)
+    const tasks = response.data.task
+
+    const ul = document.getElementById("task-list")
+    ul.innerHTML = ""
+
+
+    for(let i=0;i<tasks.length;i++){
+        if (tasks[i].checkbox === true){
+            continue
+        }
+
+        const li = document.createElement("li")
+
+        const checkbox = document.createElement("input")
+        checkbox.type = "checkbox"
+        checkbox.checked = tasks[i].checkbox === true
+
+        const label = document.createElement("span")
+        label.textContent = " " + tasks[i].content
+
+        checkbox.addEventListener("change", async function(){
+            if(checkbox.checked){
+                await axios.delete("http://localhost:3000/task/" + tasks[i]._id)
+                li.remove()
+            }
+        })
+
+        li.append(checkbox)
+        li.append(label)
+        ul.append(li)
+    }
+
+}
+async function todo(){
+    const input = document.getElementById("text")
+    const text = input.value.trim()
+    if (!text) return
+    
+    await axios.post("http://localhost:3000/task", {
+        userId:userID,
+        content:text
+    })
+
+    input.value = ""
+    loadTask()
+}
+
+if (userID) loadTask()
